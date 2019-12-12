960787e3403b1a220f78177bd1a8100545f62b21
sroenke <sroenke@nd.edu>
2019-12-12 16:20:34 -0600

    adding exercise code

--- exercise10.R
+++ exercise10.R
@@ -4,7 +4,7 @@ library("cowplot")
 library("reshape2")
 
 #set initial vectors/parameters
-N<-100
+N<-99
 M<-1
 r<-0.1
 rdN<-(-0.1)
@@ -15,6 +15,7 @@ t<-2
 while (t<1000) {
   N[t]<-N[t-1]+r*N[t-1]*(1-(N[t-1]+M[t-1])/K)
   M[t]<-M[t-1]+r*M[t-1]*(1-(N[t-1]+M[t-1])/K)
+  #if else to stop the simulation when equilibrium is reached
   if (abs(N[t-1]-N[t])>0.1 || abs(M[t-1]-M[t])>0.05){
     t<-t+1
   }
@@ -22,11 +23,11 @@ while (t<1000) {
     t<-t+1000
   }
 }
-
+#simulation with drug-induced growthrates 
   for (t in (length(N)+1):(length(N)+400)){
     N[t]<-N[t-1]+rdN*N[t-1]*(1-(N[t-1]+M[t-1])/K)
     M[t]<-M[t-1]+rdM*M[t-1]*(1-(N[t-1]+M[t-1])/K)
   }
-  
-data<-data.frame(t=1:length(M),M=M,N=N)
-ggplot(data=data,mapping=aes(x=t, y=N))+geom_line(aes(y=data$M,x=data$t),col='red')+geom_line(aes(y=data$N,x=data$t),col='black')
+#put data together for plotting
+data<-data.frame(time=1:length(M),Mutant=M,Normal=N)
+ggplot(data=data,mapping=aes(x=time, y=Normal))+geom_line(aes(y=data$Mutant,x=data$t),col='red')+geom_line(aes(y=data$Normal,x=data$t),col='black')+theme(legend.position = "top")
